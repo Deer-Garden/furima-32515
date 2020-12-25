7f051d21a7be249fc5bc679e22ec192f7e624095
Deer-Garden <willkinsongs.alais@gmail.com>
2020-12-25 14:46:02 +0900

    コメントの削除

--- db/migrate/20201222053837_create_addresses.rb
+++ db/migrate/20201222053837_create_addresses.rb
@@ -3,9 +3,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string :postal_code,        default: "", null: false
       t.integer :prefecture,                     null: false
-      t.string :city,               default: ""
-      t.string :street,             default: ""
-      t.string :build,              default: ""
+      t.string :city,               default: "",  null: false
+      t.string :street,             default: "",  null: false
+      t.string :build,              default: "",  null: false
       t.string :phone,                           null: false
       t.references :order,                        null: false, foreign_key: true
       t.timestamps
